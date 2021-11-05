0f857372376977e13d3f9e6b6761e9cb646f2035
vista136 <avistakautsar01@gmail.com>
2021-11-05 21:19:16 +0700

    React Native Video Custom

---
+++ android-exoplayer/src/main/java/com/vista/react/ReactVideoPackage.java
@@ -0,0 +1,44 @@
+package com.visa.react;
+
+import com.vista.exoplayer.DefaultReactExoplayerConfig;
+import com.vista.exoplayer.ReactExoplayerConfig;
+import com.vista.exoplayer.ReactExoplayerViewManager;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.Collections;
+import java.util.List;
+
+public class ReactVideoPackage implements ReactPackage {
+
+    private ReactExoplayerConfig config;
+
+    public ReactVideoPackage() {
+    }
+
+    public ReactVideoPackage(ReactExoplayerConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+
+    // Deprecated RN 0.47
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        if (config == null) {
+            config = new DefaultReactExoplayerConfig(reactContext);
+        }
+        return Collections.singletonList(new ReactExoplayerViewManager(config));
+    }
+}
---
+++ android-exoplayer/src/main/java/com/vista/receiver/BecomingNoisyListener.java
@@ -0,0 +1,13 @@
+package com.vista.receiver;
+
+public interface BecomingNoisyListener {
+
+    BecomingNoisyListener NO_OP = new BecomingNoisyListener() {
+        @Override public void onAudioBecomingNoisy() {
+            // NO_OP
+        }
+    };
+
+    void onAudioBecomingNoisy();
+
+}
